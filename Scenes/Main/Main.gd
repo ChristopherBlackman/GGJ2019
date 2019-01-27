9a8df8d00a19f1d82d77f7367163d6905df63454
Christopher Blackman <ChristopherBlackman@users.noreply.github.com>
2019-01-27 00:31:13 -0500

    added optoin to add cards to cycle

--- Scenes/Main/Main.gd
+++ Scenes/Main/Main.gd
@@ -1,27 +1,26 @@
 extends Control
 
 var root_node = self
-
-const base_path = "res://Scenes/Cards/Card.tscn"
-var logger = null
-
 var next_cycle_additions = []
 
-func _ready():
-	logger = $GUI/Frame/View/VBoxContainer/Logger
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	
-	#var base_card = card_scene.instance()
+#------------------------------------------------------------------------------
+#
+#
+#	Definitions 
+#
+#
+#------------------------------------------------------------------------------
 
-	#deck.append(base_card)
-	
+func _ready():
 	# this is an example on how to add a card 
 	print("loading")
-	$GUI/Frame/Cards.add_child(create_card(base_path))
+	
+	#this is an example for adding a card to the tree
+	#var base_path = "res://Scenes/Cards/Card.tscn"
+	#$GUI/Frame/Cards.add_child(create_card(base_path))
 	
 	# this is an example for updateing all the cards for the turn
-	get_tree().call_group("Cards","next_cycle")
+	#get_tree().call_group("Cards","next_cycle")
 
 #group call : cards call this to add on next cycle
 func add_card_to_next_cycle(path):
@@ -30,12 +29,15 @@ func add_card_to_next_cycle(path):
 	next_cycle_additions.append(card)
 	print(next_cycle_additions)
 
-
+# instansiates a card from a path
 func create_card(path):
 	var card_res = load(path)
 	var card = card_res.instance()
-	card.connect("send_log_msg",logger,"log_msg")
 	return card
+
+# adds new cards to deck, simulates a cycle
+func next_cycle():
+	get_tree().call_group("Cards","next_cycle")
+	while len(next_cycle_additions) > 0:
+		$GUI/Frame/Cards.add_child(next_cycle_additions.pop_front(),true)
 	
-	
-#func spawn_deck():
