6a0b2a2a3e6b81dfab8173eec4f01a2d6cdf54d8
Forest <wizcardforest@gmail.com>
2019-01-27 14:19:52 -0500

    Card scrolling

--- Scenes/Cards/CardsHand.gd
+++ Scenes/Cards/CardsHand.gd
@@ -6,18 +6,21 @@ func _ready():
 	children = self.get_children()
 	
 func _process(delta):
-	var currX = get_local_mouse_position().x
-	var currY = get_local_mouse_position().y
+	var currX = get_global_mouse_position().x
+	var currY = get_global_mouse_position().y
 	
+	print(str(OS.get_real_window_size().x) + ", " + str(currX))
+	children = self.get_children()
 	if currX > 0 and currX < self.get_size().x and currY > 0 and currY < self.get_size().y:
-		children = self.get_children()
-	
 		for child in children:
 			child.set_custom_minimum_size(Vector2(100, 130)) 
 			child.set_size(Vector2(100, 130))
+			
+		var screen_x = OS.get_real_window_size().x
+		var num_card_len = children.size() * 100
+		var x_position = 50 - num_card_len * (currX / screen_x)
+		self.set_position(Vector2(x_position, 0))
 	else:
-		children = self.get_children()
-		
 		for child in children:
 			child.set_custom_minimum_size(Vector2(50, 130)) 
 			child.set_size(Vector2(100, 130))
